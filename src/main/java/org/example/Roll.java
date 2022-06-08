91151b6393ca91882ecce44f2a88a9428fd6f4d3
bessonm <besson.mael@gmail.com>
2022-06-08 14:25:46 +0200

    tech: Try new approach

---
+++ src/main/java/org/example/Roll.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Roll {
+
+    final int pinDown;
+
+    public Roll(int pinDown) {
+        this.pinDown = pinDown;
+    }
+
+    public int score() {
+        return 0;
+    }
+}
