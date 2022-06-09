94bfe1b7fcf449f156714ced93be702f18334b99
Adrien GOGOIS <ad.gogois@gmail.com>
2022-06-09 14:29:28 +0200

    Refactor 2 tests to a rollMany() method

--- src/test/java/org/example/BowlingGameTest.java
+++ src/test/java/org/example/BowlingGameTest.java
@@ -9,22 +9,21 @@ public class BowlingGameTest {
 
     @Test
     public void testGutterGame() throws Exception {
-
         int n = 20;
         int pins = 0;
-        for (int i = 0; i < n; i++) {
-            g.roll(pins);
-        }
+        rollMany(20, 0);
         Assertions.assertThat(g.score()).isEqualTo(0);
 
     }
 
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++)
+            g.roll(pins);
+    }
+
     @Test
     public void testAllOnes() throws Exception {
-
-        for (int i = 0; i < 20; i++) {
-            g.roll(1);
-        }
+        rollMany(20, 1);
         Assertions.assertThat(g.score()).isEqualTo(20);
 
     }
