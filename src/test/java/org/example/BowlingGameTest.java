84bf9460ae7003c08363847f0d6b9ee6e32f150f
Adrien GOGOIS <ad.gogois@gmail.com>
2022-06-09 14:26:34 +0200

    Second test

--- src/test/java/org/example/BowlingGameTest.java
+++ src/test/java/org/example/BowlingGameTest.java
@@ -5,13 +5,28 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BowlingGameTest {
+    Game g = new Game();
 
     @Test
     public void testGutterGame() throws Exception {
-        Game g = new Game();
-        for (int i=0; i<20; i++)
-            g.roll(0);
+
+        int n = 20;
+        int pins = 0;
+        for (int i = 0; i < n; i++) {
+            g.roll(pins);
+        }
         Assertions.assertThat(g.score()).isEqualTo(0);
+
+    }
+
+    @Test
+    public void testAllOnes() throws Exception {
+
+        for (int i = 0; i < 20; i++) {
+            g.roll(1);
+        }
+        Assertions.assertThat(g.score()).isEqualTo(20);
+
     }
 
 }
